1de5c2563e380f7904aeeefcdae541bae04baf73
LC1B_21_ホリケ_ハヤト <k024g0019@g.neec.ac.jp>
2025-04-24 13:24:28 +0900

    imgui.iniが排除されているかの確認

--- main.cpp
+++ main.cpp
@@ -1265,7 +1265,6 @@ Matrix4x4 MakePerspectiveForMatrix(float fovY, float aspectRatio, float nearClip
 	return result;
 }
 
-
 Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip) {
 
 	Matrix4x4 result;
@@ -1291,6 +1290,7 @@ Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float botto
 
 	return result;
 }
+
 Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth) {
 
 	Matrix4x4 result;
