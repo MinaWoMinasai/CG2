212f4f810c7ea3d9199ae9e322e118fbea265d35
LC1B_21_ホリケ_ハヤト <k024g0019@g.neec.ac.jp>
2026-02-09 12:43:33 +0900

    AL4ゲーム提出

--- project/game/scene/Game.h
+++ project/game/scene/Game.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <memory>
 #include "SceneManager.h"
+#include "RenderTexture.h"
+#include "PostEffect.h"
 
 class Game {
 public:
@@ -18,4 +20,17 @@ private:
     std::unique_ptr<DirectXCommon> dxCommon_;
     std::unique_ptr<SrvManager> srvManager_;
     std::unique_ptr<SceneManager> sceneManager_;
+    std::unique_ptr<RtvManager> rtvManager_;
+    std::unique_ptr<RenderTexture> sceneRenderTexture_;
+    std::unique_ptr<BloomConstantBuffer> bloomCB_;
+    std::unique_ptr<PostEffect> postEffect_;
+    std::unique_ptr<RenderTexture> bloomRT_A_;
+    std::unique_ptr<RenderTexture> bloomRT_B_;
+    std::unique_ptr<RenderTexture> bloomRT_Half_;
+    BloomParam bloomParam_{};
+
+
+
+    void TransitionResource(DirectXCommon* dxCommon, ID3D12Resource* resource, D3D12_RESOURCE_STATES stateBefore, D3D12_RESOURCE_STATES stateAfter);
+
 };
